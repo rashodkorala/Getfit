d2d5013c836566132ad8aae56724f088ae2b08d8
rashodk <rmanamperiko@mun.ca>
2023-12-02 15:36:10 -0330

    update routing and ui

--- lib/view/createNewWorkout_view.dart
+++ lib/view/createNewWorkout_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getfit/controller/prebuiltWorkoutService.dart';
 import 'package:getfit/model/workoutExercise_model.dart';
+import 'package:getfit/view/ViewWorkoutPlanPage.dart';
 import '../controller/exerciesService.dart';
 import '../controller/workoutService.dart';
 import '../model/exercise_model.dart';
@@ -75,24 +76,32 @@ class _CreateIndividualWorkoutPageState
     );
 
     try {
-      if (widget.isEditing == true) {
-        // Update existing workout
-        await _workoutService.updateWorkout(widget.workout!.id, newWorkout);
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Workout updated successfully!')),
-        );
-      } else {
-        // Save new workout
-        if (widget.destination == 'prebuilt') {
-          await _prebuiltWorkoutService.addPrebuiltWorkout(newWorkout);
-        } else {
-          await _workoutService.addWorkout(newWorkout);
-        }
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Workout saved successfully!')),
-        );
-      }
-      Navigator.pop(context);
+      // if (widget.isEditing == true) {
+      //   // Update existing workout
+      //   await _workoutService.updateWorkout(widget.workout!.id, newWorkout);
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     const SnackBar(content: Text('Workout updated successfully!')),
+      //   );
+      // } else {
+      //   // Save new workout
+      //   if (widget.destination == 'prebuilt') {
+      //     await _prebuiltWorkoutService.addPrebuiltWorkout(newWorkout);
+      //   } else {
+      //     await _workoutService.addWorkout(newWorkout);
+      //   }
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     const SnackBar(content: Text('Workout saved successfully!')),
+      //   );
+      // }
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ViewWorkoutPlanPage(
+            workout: newWorkout,
+            isprebuilt: false,
+          ),
+        ),
+      ).then((value) => Navigator.pop(context));
     } catch (e) {
       print('Error saving workout: $e');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -105,9 +114,12 @@ class _CreateIndividualWorkoutPageState
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.isEditing == true
-            ? 'Edit Workout Plan'
-            : 'Create Workout Plan'),
+        title: Text(
+          widget.isEditing == true
+              ? 'Edit Workout Plan'
+              : 'Create Workout Plan',
+          style: TextStyle(fontSize: 18),
+        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.save),
@@ -120,15 +132,42 @@ class _CreateIndividualWorkoutPageState
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Workout Plan Name:'),
-            TextField(
-              controller: workoutNameController,
-              decoration: const InputDecoration(
-                hintText: 'Enter workout plan name',
+            SizedBox(
+              height: 50,
+              child: TextFormField(
+                controller: workoutNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Workout Name',
+                ),
               ),
             ),
             const SizedBox(height: 16.0),
-            const Text('Selected Exercises:'),
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: _addExercise,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 40),
+                  backgroundColor: ThemeData().colorScheme.primary,
+                  foregroundColor: ThemeData().colorScheme.onSecondary,
+                ),
+                // ignore: prefer_const_constructors
+                child: Row(
+                  mainAxisSize: MainAxisSize
+                      .min, // To keep the row size just as big as its children
+                  children: const [
+                    // Spacing between icon and text
+                    Text('Add Exercise'),
+                    SizedBox(width: 8),
+                    Icon(
+                      Icons.add,
+                      size: 20,
+                    ), // Replace with your desired icon
+                  ],
+                ),
+              ),
+            ),
             Expanded(
               child: ListView.builder(
                 itemCount: selectedExercises.length,
@@ -141,10 +180,6 @@ class _CreateIndividualWorkoutPageState
                   },
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: _addExercise,
-              child: const Text('Add Exercise'),
             ),
           ],
         ),
@@ -229,7 +264,7 @@ class _ExerciseTileState extends State<ExerciseTile> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.all(12.0),
+      // margin: const EdgeInsets.all(2.0),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
@@ -238,21 +273,29 @@ class _ExerciseTileState extends State<ExerciseTile> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  capitalizeFirstLetterOfEachWord(widget.exercise.name),
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.w500),
+                Flexible(
+                  child: Text(
+                    capitalizeFirstLetterOfEachWord(widget.exercise.name),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    overflow: TextOverflow
+                        .visible, // Allow text to wrap to the next line
+                    softWrap: true, // Enable text wrapping
+                  ),
                 ),
                 IconButton(
-                    onPressed: () {
-                      showExerciseDialog(context, widget.exercise);
-                    },
-                    icon: const Icon(
-                      Icons.question_mark,
-                      size: 20,
-                    )),
+                  onPressed: () {
+                    showExerciseDialog(context, widget.exercise);
+                  },
+                  icon: const Icon(
+                    Icons.question_mark,
+                    size: 18,
+                  ),
+                ),
                 IconButton(
-                  icon: const Icon(Icons.remove_circle_outline, size: 20),
+                  icon: const Icon(Icons.remove_circle_outline, size: 18),
                   onPressed: widget.onRemove,
                 ),
               ],
@@ -277,9 +320,9 @@ class _ExerciseTileState extends State<ExerciseTile> {
               children: [
                 const TableRow(
                   children: [
-                    Text('Set'),
-                    Text('Weight (lb)'),
-                    Text('Reps'),
+                    Text('Set', textAlign: TextAlign.start),
+                    Text('Weight (lb)', textAlign: TextAlign.center),
+                    Text('Reps', textAlign: TextAlign.center),
                     SizedBox.shrink(),
                   ],
                 ),
@@ -287,11 +330,11 @@ class _ExerciseTileState extends State<ExerciseTile> {
                   return TableRow(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        child: Text('${index + 1}'),
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('${index + 1}', textAlign: TextAlign.start),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
                         child: TextFormField(
                           initialValue:
                               '${widget.exercise.sets[index].weight == 0 ? '' : widget.exercise.sets[index].weight}',
@@ -303,15 +346,16 @@ class _ExerciseTileState extends State<ExerciseTile> {
                                 value.isEmpty ? 0 : int.parse(value);
                           }),
                           decoration: const InputDecoration(
+                            suffixText: 'lb',
                             isDense: true,
                             contentPadding: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 8),
+                                horizontal: 8, vertical: 2),
                           ),
                           style: const TextStyle(fontSize: 14),
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
                         child: TextFormField(
                           initialValue:
                               '${widget.exercise.sets[index].reps == 0 ? '' : widget.exercise.sets[index].reps}',
@@ -322,15 +366,16 @@ class _ExerciseTileState extends State<ExerciseTile> {
                                 value.isEmpty ? 0 : int.parse(value);
                           }),
                           decoration: const InputDecoration(
+                            suffixText: '',
                             isDense: true,
                             contentPadding: EdgeInsets.symmetric(
-                                horizontal: 8, vertical: 8),
+                                horizontal: 5, vertical: 2),
                           ),
                           style: const TextStyle(fontSize: 14),
                         ),
                       ),
                       IconButton(
-                        icon: const Icon(Icons.remove_circle_outline),
+                        icon: const Icon(Icons.remove_circle_outline, size: 18),
                         onPressed: () => _removeSet(index),
                       ),
                     ],
